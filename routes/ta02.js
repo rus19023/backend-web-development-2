13b91af32c8c3eeec7d9a5072cbc71045f0f0cf5
Braiden Parkinson <braidencparkinson@gmail.com>
2021-09-17 18:33:11 -0600

    working

--- routes/ta02.js
+++ routes/ta02.js
@@ -14,6 +14,7 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/form", (req, res, next) => {
+  console.log(users);
   res.render("pages/ta02", { path: "/form", users, title: "form" });
 });
 
